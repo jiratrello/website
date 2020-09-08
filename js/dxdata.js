d2b8da85b38939e7fc79daca4b86811b545868bf
troy <mail@troyduguid.com>
2020-09-08 17:43:26 +0200

    jazz help meee

---
+++ js/dxdata.js
@@ -0,0 +1,22 @@
+const dxData = [
+    {
+        name: 'Utopias: Navigating Without Coordinates',
+        description: 'Is a collective utopia possible?',
+        year: '2020',
+    },
+    {
+        name: 'Data Mutations',
+        description: 'We mutate each other',
+        year: '2017',
+    },
+    {
+        name: 'Homezome',
+        description: 'Build crap',
+        year: '2016',
+    },
+    {
+        name: 'Dystropicana',
+        description: 'Shits fucked',
+        year: '2016',
+    },
+];
--- js/dxselector.js
+++ js/dxselector.js
@@ -1,14 +1,13 @@
+let dxDataElements = [];
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 1000 );
-
 const renderer = new THREE.WebGLRenderer( /*{ alpha: true }*/ );
-
-
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
+scene.add( cube );
 camera.position.z = 5;
 
 window.onload = function() {
@@ -29,6 +28,21 @@ window.onload = function() {
 	});
 
 	animate();
+
+	for(let i = 0; i < dxData.length; i += 1) {
+		dxDataElements.push(createDxElement(dxData[i], 100, 100+(i*20)));
+	}
+}
+
+function createDxElement(dxData, x, y) {
+	let parentelement = document.createElement("div");
+	parentelement.innerHTML = dxData.name;
+	parentelement.className = "dxElement";
+	//'px' to convert to pixels
+	parentelement.style.left = x+'px';
+	parentelement.style.top = y+'px';
+	document.body.appendChild(parentelement);
+	return parentelement;
 }
 
 function animate() {
@@ -36,4 +50,16 @@ function animate() {
 	renderer.render( scene, camera );
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
+}
+
+function menuAnimate() {
+	requestAnimationFrame (menuAnimate);
+	let length = 100;
+	let rate = 0.001;
+	let offset = 300;
+	for(let i = 0; i < dxDataElements.length; i += 1) {
+		let x = Math.sin(i+Date.now()*rate)*length+offset;
+		dxDataElements[i].style.left = x+'px';
+		// dxDataElements[i].style.top = y+'px';
+	}
 }
