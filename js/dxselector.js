5ed3cbb8003da05ceada77847892c296a83e459c
troy <mail@troyduguid.com>
2020-09-08 19:16:55 +0200

    spinning words

--- js/dxselector.js
+++ js/dxselector.js
@@ -1,8 +1,9 @@
 let dxDataElements = [];
+let debugSVG = null;
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 1000 );
-const renderer = new THREE.WebGLRenderer( /*{ alpha: true }*/ );
+const renderer = new THREE.WebGLRenderer( { alpha: true } );
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
@@ -27,16 +28,22 @@ window.onload = function() {
 		camera.updateProjectionMatrix();
 	});
 
-	animate();
-
 	for(let i = 0; i < dxData.length; i += 1) {
 		dxDataElements.push(createDxElement(dxData[i], 100, 100+(i*20)));
 	}
+
+	animate();
+	menuAnimate();
+}
+
+function hackerMode(){
+	createDebugCircle(window.innerWidth / 2, window.innerHeight / 2, 350);
+	window.open("https://youtu.be/VMRaCW6OtXM");
 }
 
 function createDxElement(dxData, x, y) {
 	let parentelement = document.createElement("div");
-	parentelement.innerHTML = dxData.name;
+	parentelement.innerHTML = "<div>"+dxData.name+"</div><div>"+dxData.description+"</div>";
 	parentelement.className = "dxElement";
 	//'px' to convert to pixels
 	parentelement.style.left = x+'px';
@@ -46,7 +53,9 @@ function createDxElement(dxData, x, y) {
 }
 
 function radialPosition(angle, distance) {
-	
+	let x = Math.cos(angle)*distance;
+	let y = Math.sin(angle)*distance;
+	return {x: x, y: y};
 }
 
 function animate() {
@@ -56,14 +65,43 @@ function animate() {
 	cube.rotation.y += 0.01;
 }
 
+function createDebugCircle(x, y, radius)
+{
+	if (debugSVG == null)
+	{
+		debugSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+		debugSVG.style.position = "absolute";
+		debugSVG.setAttribute("width", "100%");
+		debugSVG.setAttribute("height", "100%");
+		document.body.appendChild(debugSVG);
+	}
+
+	let circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+
+	circle.setAttribute("cx", x);
+	circle.setAttribute("cy", y);
+	circle.setAttribute("r", radius);
+	circle.setAttribute("fill", "none");
+	circle.setAttribute("stroke", "black");
+
+	debugSVG.appendChild(circle);
+}
+
 function menuAnimate() {
 	requestAnimationFrame (menuAnimate);
-	let length = 100;
-	let rate = 0.001;
-	let offset = 300;
+	const distance = 350;
+	const rate = 0.001;
+	const centerX = window.innerWidth / 2;
+	const centerY = window.innerHeight / 2;
+	const angleDelta = (Math.PI * 2) / dxDataElements.length;
+
 	for(let i = 0; i < dxDataElements.length; i += 1) {
-		let x = Math.sin(i+Date.now()*rate)*length+offset;
-		dxDataElements[i].style.left = x+'px';
-		// dxDataElements[i].style.top = y+'px';
+		let pos = radialPosition(Date.now()*rate+i*angleDelta, distance);
+		pos.x += centerX;
+		pos.y += centerY;
+		pos.x -= dxDataElements[i].clientWidth / 2;
+		pos.y -= dxDataElements[i].clientHeight / 2;
+		dxDataElements[i].style.left = pos.x+'px';
+		dxDataElements[i].style.top = pos.y+'px';
 	}
 }
